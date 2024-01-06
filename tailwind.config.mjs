cd73b003583c94ac8706418f682279b4e51d07f8
lunoxdev <lunox.code@gmail.com>
2024-01-06 17:25:33 -0400

    Configuracion Marquee animacion

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,21 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
-	},
-	plugins: [],
-}
+		extend: {
+		  animation: {
+			marquee: "marquee var(--duration) linear infinite",
+			"marquee-vertical": "marquee-vertical var(--duration) linear infinite",
+		  },
+		  keyframes: {
+			marquee: {
+			  from: { transform: "translateX(0)" },
+			  to: { transform: "translateX(calc(-50% - var(--gap)/2))" },
+			},
+			"marquee-vertical": {
+			  from: { transform: "translateY(0)" },
+			  to: { transform: "translateY(calc(-50% - var(--gap)/2))" },
+			},
+		  },
+		},
+	  },
+	};
