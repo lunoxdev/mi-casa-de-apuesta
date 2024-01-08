f22fc9bea70bc8ffb0d21666bee2b521a89a5b04
lunoxdev <lunox.code@gmail.com>
2024-01-08 10:37:10 -0400

    Se agrega animacion para el boton Promo Code

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,22 +1,31 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {
-		  animation: {
-			marquee: "marquee var(--duration) linear infinite",
-			"marquee-vertical": "marquee-vertical var(--duration) linear infinite",
-		  },
-		  keyframes: {
-			marquee: {
-			  from: { transform: "translateX(0)" },
-			  to: { transform: "translateX(calc(-50% - var(--gap)/2))" },
-			},
-			"marquee-vertical": {
-			  from: { transform: "translateY(0)" },
-			  to: { transform: "translateY(calc(-50% - var(--gap)/2))" },
-			},
-		  },
-		},
-	  },
-	};
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      animation: {
+        marquee: "marquee var(--duration) linear infinite",
+        "marquee-vertical": "marquee-vertical var(--duration) linear infinite",
+        "background-shine": "background-shine 2s linear infinite",
+      },
+      keyframes: {
+        marquee: {
+          from: { transform: "translateX(0)" },
+          to: { transform: "translateX(calc(-50% - var(--gap)/2))" },
+        },
+        "marquee-vertical": {
+          from: { transform: "translateY(0)" },
+          to: { transform: "translateY(calc(-50% - var(--gap)/2))" },
+        },
+        "background-shine": {
+          from: {
+            backgroundPosition: "0 0",
+          },
+          to: {
+            backgroundPosition: "-200% 0",
+          },
+        },
+      },
+    },
+  },
+};
