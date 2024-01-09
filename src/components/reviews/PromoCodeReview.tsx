e217d86367129e555e823934a5f3a0cbf145bdcb
lunoxdev <lunox.code@gmail.com>
2024-01-08 21:22:49 -0400

    Se crea Boton PromoCode para el review

---
+++ src/components/reviews/PromoCodeReview.tsx
@@ -0,0 +1,27 @@
+import { useState } from "react";
+
+const PromoCode = ({ ...info }) => {
+  const [copied, setCopied] = useState(false);
+
+  const handleCopyClick = () => {
+    navigator.clipboard.writeText(info.promocode);
+    setCopied(true);
+
+    setTimeout(() => {
+      setCopied(false);
+    }, 2000);
+  };
+
+  return (
+    <button
+      className="inline-flex w-20 h-10 animate-background-shine items-center justify-center rounded-sm border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] font-medium transition-colors hover:border-slate-600 hover:border-sm"
+      onClick={handleCopyClick}
+    >
+      <p className="hover:scale-110 w-full uppercase text-sm">
+        {copied ? "Copiado" : info.promocode}
+      </p>
+    </button>
+  );
+};
+
+export default PromoCode;
