fc0401b2424b591ca9d720fb9e7f1e78550c9ed3
lunoxdev <lunox.code@gmail.com>
2024-01-11 23:01:11 -0400

    Se mejora el diseno

--- src/components/reviews/PromoCodeReview.tsx
+++ src/components/reviews/PromoCodeReview.tsx
@@ -13,14 +13,16 @@ const PromoCode = ({ ...info }) => {
   };
 
   return (
-    <button
-      className="inline-flex w-20 h-10 animate-background-shine items-center justify-center rounded-sm border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] font-medium transition-colors hover:border-slate-600 hover:border-sm"
-      onClick={handleCopyClick}
-    >
-      <p className="hover:scale-110 w-full uppercase text-sm">
-        {copied ? "Copiado" : info.promocode}
-      </p>
-    </button>
+    <>
+      <button
+        className="inline-flex w-1/2 h-12 mt-2 text-lg animate-background-shine items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] font-medium transition-colors hover:border-slate-600 hover:border-sm"
+        onClick={handleCopyClick}
+      >
+        <p className="hover:scale-110 uppercase">
+          {copied ? "Copiado" : info.promocode}
+        </p>
+      </button>
+    </>
   );
 };
 
