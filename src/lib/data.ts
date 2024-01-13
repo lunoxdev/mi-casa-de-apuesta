4c67feb8aa0ca2cdeff0f86c62533240a1c3aa0b
lunoxdev <lunox.code@gmail.com>
2024-01-13 14:27:08 -0400

    Se agregan iconoes devices a la data

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -1,39 +1,65 @@
 export interface Operator {
-    id: number;
-    name: string;
-    img: string;
-    rating: number;
-    promocode: string;
-    urlregistro: string;
-    urlbono: string;
-    email: string;
-    phone: string;
-    license: string;
-  }
-  
-  export const operators: Operator[] = [
-    {
-      id: 1,
-      name: "Stake",
-      img: "operators/withbg/stake.svg",
-      rating: 3.5,
-      promocode: "XXXXXX",
-      urlregistro: "https://moisesobando.com/",
-      urlbono: "https://moisesobando.com/",
-      email: "ayuda@operador.com",
-      phone: "+000 00000000",
-      license: "Nombre de la licencia"
-    },
-    {
-      id: 2,
-      name: "Betano",
-      img: "operators/withbg/betano.svg",
-      rating: 4.5,
-      promocode: "YYYYYY",
-      urlregistro: "https://moisesobando.com/",
-      urlbono: "https://moisesobando.com/",
-      email: "ayuda@operador.com",
-      phone: "+000 00000000",
-      license: "Nombre de la licencia"
-    },
-  ];
+  id: number;
+  name: string;
+  img: string;
+  rating: number;
+  promocode: string;
+  urlregistro: string;
+  urlbono: string;
+  email: string;
+  phone: string;
+  license: string;
+  apps?: {
+    android?: string;
+    ios?: string;
+    webapp?: string;
+    nameandroid?: string;
+    nameios?: string;
+    nameweb?: string;
+  }[];
+}
+
+export const operators: Operator[] = [
+  {
+    id: 1,
+    name: "Stake",
+    img: "operators/withbg/stake.svg",
+    rating: 3.5,
+    promocode: "XXXXXX",
+    urlregistro: "https://moisesobando.com/",
+    urlbono: "https://moisesobando.com/",
+    email: "ayuda@operador.com",
+    phone: "+000 00000000",
+    license: "Nombre de la licencia",
+    apps: [
+      {
+        android: "/devices/android.svg",
+        nameandroid: "Android",
+        ios: "/devices/ios.svg",
+        nameios: "iPhone",
+        webapp: "/devices/phone.svg",
+        nameweb: "Aplicación Web",
+      },
+    ],
+  },
+  {
+    id: 2,
+    name: "Betano",
+    img: "operators/withbg/betano.svg",
+    rating: 4.5,
+    promocode: "YYYYYY",
+    urlregistro: "https://moisesobando.com/",
+    urlbono: "https://moisesobando.com/",
+    email: "ayuda@operador.com",
+    phone: "+000 00000000",
+    license: "Nombre de la licencia",
+    apps: [
+      {
+        android: "/devices/android.svg",
+        nameandroid: "Android",
+        webapp: "/devices/phone.svg",
+        nameweb: "Aplicación Web",
+      },
+    ],
+  },
+];
