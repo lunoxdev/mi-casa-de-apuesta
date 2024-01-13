649a1048a355948584955656e278fbe58b251a10
lunoxdev <lunox.code@gmail.com>
2024-01-12 22:58:47 -0400

    Se crea el CountUp de los Operadores

---
+++ src/components/reviews/CountUp.tsx
@@ -0,0 +1,18 @@
+import { useProgressiveNumber } from '../../hooks/useProgressiveNumber';
+import { useEffect } from 'react';
+
+export const CountUp = ({ initial, final, duration, showPlusSign = true }: { initial: number, final: number, duration?: number, showPlusSign?: boolean }) => {
+  const [count, setCount] = useProgressiveNumber(initial, duration);
+
+  useEffect(() => {
+    setCount(final);
+  }, []);
+
+  const formattedCount = new Intl.NumberFormat().format(count); // Formatea el número con la coma
+
+  return (
+    <p className="text-4xl sm:text-5xl font-semibold">
+      {showPlusSign ? `+${formattedCount}` : formattedCount}
+    </p>
+  );
+};
